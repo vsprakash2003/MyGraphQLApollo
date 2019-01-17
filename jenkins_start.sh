7408f142f88c1ce86a04fdd986656c8e380b4db4
Mycents <prakashsubramani@hotmail.com>
2019-01-17 16:05:09 -0500

    Jenkins docker files

---
+++ jenkins_start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#
+# Launch docker-compose with the database initiated
+#
+
+cd $(dirname $0)
+
+# Make sure we close out the previous containers
+docker-compose -f docker-compose.jenkins.yml down
+
+# This needs to be run twice some times. 
+docker-compose -f docker-compose.jenkins.yml down
+
+# Rebuild the image (required if dependencies change)
+docker-compose -f docker-compose.jenkins.yml -p Jenkins up --build -d
