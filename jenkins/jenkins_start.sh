f8d8b2f5184b50aed10c08df93f90c6340897952
Mycents <prakashsubramani@hotmail.com>
2019-01-24 11:14:35 -0500

    Updated jenkinsfile and docker-compose.jenkins as jenkinsci-blueocean image is failing

--- jenkins/jenkins_start.sh
+++ jenkins/jenkins_start.sh
@@ -4,6 +4,7 @@
 # Launch docker-compose with the database initiated
 #
 
+echo "hi"
 cd $(dirname $0)
 
 # Make sure we close out the previous containers
@@ -12,5 +13,6 @@ docker-compose -f docker-compose.jenkins.yml down
 # This needs to be run twice some times. 
 docker-compose -f docker-compose.jenkins.yml down
 
+echo "hiya"
 # Rebuild the image (required if dependencies change)
-docker-compose -f docker-compose.jenkins.yml -p Jenkins up --build -d
+docker-compose -f docker-compose.jenkins.yml up --build -d
